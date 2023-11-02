c52989cc3b282e2cd1ff3efffd3ca5de786bad63
Laaro Babatunde <99040855+Therealmohh@users.noreply.github.com>
2023-11-02 14:16:51 -0700

    Delete images/script.js

--- images/script.js
+++
@@ -1,6 +0,0 @@
-const hamburger = document.getElementsByClassName("hamburger")[0];
-const mobile = document.getElementsByClassName("container")[0];
-
-hamburger.addEventListener("click", () => {
-  mobile.classList.toggle("active");
-});
